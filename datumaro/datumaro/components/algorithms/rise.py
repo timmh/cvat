ffa384a7db246e9bb121c3571eb8789bf0c803f6
Zhiltsov Max <zhiltsov.max35@gmail.com>
2020-06-10 20:19:12 +0300

    fix import

--- datumaro/datumaro/components/algorithms/rise.py
+++ datumaro/datumaro/components/algorithms/rise.py
@@ -9,7 +9,7 @@ import numpy as np
 from math import ceil
 
 from datumaro.components.extractor import AnnotationType
-from datumaro.utils.annotation_tools import nms
+from datumaro.util.annotation_tools import nms
 
 
 def flatmatvec(mat):
